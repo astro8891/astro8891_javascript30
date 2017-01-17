5063d36fc03eaa90ee8dc330dca571a7bfaec67b
Ashley Stewart <Ashley Stewart>
2017-01-17 23:47:34 +1300

    keybinding implimented

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 
     window.addEventListener('keydown', function (e) {
-        console.log(e);
+        const audio = document.querySelector(`audio[data-key=${e.keyCode}]`)
     })
-
 });
