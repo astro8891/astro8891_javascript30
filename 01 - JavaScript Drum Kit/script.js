3fb65e00f19b2dd5c4b08c1a77e392cff86926a2
Ashley Stewart <Ashley Stewart>
2017-01-23 02:14:00 +1300

    all features working now

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,10 +1,20 @@
 $(document).ready(function () {
     window.addEventListener('keydown', function (e) {
         const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-        const key = document.querySelector(`key[data-key="${e.keyCode}"]`);
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
         if (!audio) return;
         audio.currentTime = 0; //rewind the key to the start, can hit key multiple time
         audio.play();
-        $(key).addClass('playing');
-    })
+        key.classList.add('playing');
+    });
+
+    function removeTransition(e) {
+        if (e.propertyName !== 'transform')
+            return;
+        this.classList.remove('playing');
+        console.log(this);
+    }
+
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 });
